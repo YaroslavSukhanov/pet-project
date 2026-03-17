404ce62ff3eb7f720da8a8a45aa67d7da48de1a4
YaroslavSukhanov <suxanofficial@gmail.com>
2026-03-17 20:48:54 +0100

    feat(infra): hot module replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({paths, isDev}: BuildOptions): Array<webpack.Webpac
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ];
 }
