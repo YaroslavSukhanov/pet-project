bbbd91b9174228e331d2ec3cecedc396988a682c
YaroslavSukhanov <suxanofficial@gmail.com>
2026-05-13 17:31:48 +0200

    feat(infra): screenshot tests

---
+++ config/storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from '@storybook/react-webpack5';
+import { StyleDecorator } from 'shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecoorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
+import 'shared/config/i18n/i18n';
+
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+        layout: 'fullscreen',
+    },
+    decorators: [StyleDecorator, ThemeDecorator(Theme.Normal), RouterDecorator],
+};
+
+export default preview;
---
+++ config/storybook/test-runner.ts
@@ -0,0 +1,19 @@
+import { TestRunnerConfig } from '@storybook/test-runner';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
+
+const config: TestRunnerConfig = {
+    setup() {
+        expect.extend({ toMatchImageSnapshot });
+    },
+    async postVisit(page, context) {
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot({
+            customSnapshotsDir,
+            customSnapshotIdentifier: context.id,
+        });
+    },
+};
+
+export default config;
